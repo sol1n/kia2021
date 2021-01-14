75ebf12648cb5f08fd804a4edc6e23e013e34397
Artem Garnyshev <is.perfect.possible@gmail.com>
2021-01-14 16:27:15 +0300

    Сообщение об успешной регистрации

--- src/helpers/message.js
+++ src/helpers/message.js
@@ -1,10 +1,16 @@
-const successModal = document.getElementById('success-modal');
+const successModal = document.getElementById('success-message'),
+    form = document.getElementById('register-form');
 
-export const hideLoader = function () {
-    loader.classList.add('hidden');
+export const hideSuccessMessage = function () {
+    form.classList.remove('hidden');
+    form.hidden = false;
+    successModal.classList.add('hidden');
+    successModal.hidden = true;
 }
 
 export const showSuccessMessage = function() {
+    form.classList.add('hidden');
+    form.hidden = true;
     successModal.classList.remove('hidden');
     successModal.hidden = false;
 }
