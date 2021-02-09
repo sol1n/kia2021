b460be7618ed0df35dac0e5f5820e5e7a3dff815
Tysren <tsyrya@gmail.com>
2021-02-09 19:41:36 +0800

    region combobox

---
+++ src/helpers/select.js
@@ -0,0 +1,23 @@
+import Choices from "choices.js";
+
+const initRegionSelect = (data) => {
+    var select = document.querySelector('[name="rehion"]');
+    if (select) {
+        new Choices(select, {
+            placeholder: true,
+            placeholderValue: "Выберите регион",
+            choices: data.map(item => {
+                return {
+                    value: item.id,
+                    label: item.title
+                }
+            }),
+            searchEnabled: false,
+            itemSelectText: ''
+        })
+    }
+}
+
+export {
+    initRegionSelect
+}
